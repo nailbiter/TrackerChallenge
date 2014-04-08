cba664affae5bc44abc3bd3929c87e724b6db389
Alex Leontiev <alozz1991@gmail.com>
2014-04-08 09:03:37 +0900

    Added more flexibility and output ground truth

--- test.cpp
+++ test.cpp
@@ -6,35 +6,52 @@
 
 using namespace cv;
 
-#define RECT_H 100
-#define RECT_W 200
-#define BORDER 25
+/*TODO: read transitions from config file
+ */
+/*TODO: write this-> update contrib & opencv -> build contrib & opencv -> automate initial frame (contrib)
+ */
 
-void animate(int frames_per_period,int frames_total){
-    Mat image(468,832, CV_8U, 1);
+FILE* groundtruth=NULL;
+
+#define DEFSCREENW 832
+#define DEFSCREENH 468
+
+int animate(Point initPos, Point finalPos,int framenum,int frameOffset,
+        int screenWidth=DEFSCREENW,int screenHeight=DEFSCREENH,int objectWidth=200,int objectHeight=100){
+
+    Mat image(screenHeight,screenWidth, CV_8U, 1);
 
     char filename[50];
-    int increment=(image.cols-2*BORDER-RECT_W)/(frames_per_period-1);
+    float increment_x=(finalPos.x-initPos.x)*1.0/framenum,
+          increment_y=(finalPos.y-initPos.y)*1.0/framenum;
     
-    for(int i=0,pos=BORDER;i<frames_total;i++){
-        //printf("offset: %d\n",offset);
+    for(int i=0,px=initPos.x,py=initPos.y;i<framenum;i++,px+=increment_x,py+=increment_y){
         image=255;
-        rectangle(image,Point(pos,image.rows/2-RECT_H/2),Point(pos+RECT_W,image.rows/2+RECT_H/2),0,CV_FILLED);
-        sprintf(filename,"image%03d.jpg",i);
+        rectangle(image,Point(px,py),Point(px+objectWidth,py+objectHeight),0,CV_FILLED);
+        fprintf(groundtruth,"%d %d %d %d\n",px,py,px+objectWidth,py+objectHeight);
+        sprintf(filename,"image%03d.jpg",i+frameOffset);
         imwrite(filename,image);
-
-        if(i%(2*frames_per_period-2)<(frames_per_period-1)){
-            pos+=increment;
-        }else{
-            pos-=increment;
-        }
     }
+    return framenum;
 }
 
 int main( int arc, char ** argv)
 {
-    int total=200;
-    animate(30,total);
+    int total=0;
+    Point p1(25,468/2-100/2),
+          p2(832-25-200,468/2-100/2);
+    groundtruth=fopen("groundtruth.txt","w");
+    fprintf(groundtruth,"%d %d\n",DEFSCREENH,DEFSCREENW);
+
+    total+=animate(p1,p2,50,total);
+    total+=animate(p2,p1,50,total);
+    total+=animate(p1,p2,50,total);
+    total+=animate(p2,p1,50,total);
+
+    system("ffmpeg -i image\%03d.jpg -vcodec mpeg4 test.avi");
+    system("rm -rf image*.jpg");
+
     printf("%d images written\n",total);
+    printf("ground truth is written to groundtruth.txt\n");
+    fclose(groundtruth);
 }
-//ffmpeg -i image%03d.jpg -vcodec mpeg4 test.avi
