d4133d46a0b9f9197cebc3a3eeffc716a54ec42b
Alex Leontiev <alozz1991@gmail.com>
2014-04-18 07:17:57 +0900

    Tuning

--- tracker_challenge.cpp
+++ tracker_challenge.cpp
@@ -1,5 +1,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/core/core_c.h>
+#include "opencv2/core/utility.hpp"
+#include <opencv/cv.h>
 #include <opencv2/highgui.hpp>
 #include <cstring>
 #include <cstdlib>
@@ -28,6 +30,8 @@ int animate(Point initPos, Point finalPos,int framenum,int frameOffset,
     for(int i=0,px=initPos.x,py=initPos.y;i<framenum;i++,px+=increment_x,py+=increment_y){
         image=255;
         rectangle(image,Point(px,py),Point(px+objectWidth,py+objectHeight),0,CV_FILLED);
+#define RANDO
+#ifdef RANDO
         int dec=5;
         RNG rng;
         for(int ii=0;ii<(objectWidth/dec);ii++){
@@ -36,6 +40,7 @@ int animate(Point initPos, Point finalPos,int framenum,int frameOffset,
                 rectangle(image,Point(px+ii*dec,py+jj*dec),Point(px+(ii+1)*dec,py+(jj+1)*dec),rng.next()%255,CV_FILLED);
             }
         }
+#endif
         fprintf(groundtruth,"%d %d %d %d\n",px,py,px+objectWidth,py+objectHeight);
         sprintf(filename,"image%03d.jpg",i+frameOffset);
         imwrite(filename,image);
@@ -51,14 +56,16 @@ int main( int arc, char ** argv)
     groundtruth=fopen("groundtruth.txt","w");
     fprintf(groundtruth,"%d %d\n",DEFSCREENH,DEFSCREENW);
 
-    total+=animate(p1,p2,50,total);
-    total+=animate(p2,p1,50,total);
-    total+=animate(p1,p2,50,total);
-    total+=animate(p2,p1,50,total);
-    total+=animate(p1,p2,50,total);
-    total+=animate(p2,p1,50,total);
-    total+=animate(p1,p2,50,total);
-    total+=animate(p2,p1,50,total);
+    for(int i=0;i<1;i++){
+    total+=animate(p1,p2,3+25,total);
+    total+=animate(p2,p1,9+25,total);
+    total+=animate(p1,p2,2+25,total);
+    total+=animate(p2,p1,7+25,total);
+    total+=animate(p1,p2,3+25,total);
+    total+=animate(p2,p1,4+25,total);
+    total+=animate(p1,p2,1+25,total);
+    total+=animate(p2,p1,9+25,total);
+    }
 
     system("ffmpeg -i image\%03d.jpg -vcodec mpeg4 test.avi");
     system("rm -rf image*.jpg");
